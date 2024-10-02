9ae54c366944f65146693c07eb33ecf75f616647
alexavammainen <huumonenalex@gmail.com>
2024-10-02 19:27:19 +0300

    API_KEY secret

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const app = express();
@@ -12,9 +15,24 @@ app.use(bodyParser.json());
 app.post('/commands', async(req, res)=>{
     const question = req.body.question;
     console.log(question);
+ //https://api.openai.com/v1/chat/completions muista tämä?
 
+ //se avain pitää vaihtaa jos ei toimi joku .env tiedosto tarvis olla
     try{
-        
+        const response = await fetch('https://api.openai.com/v1/chat/completions',{
+
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+
+
+            },
+            body: JSON.stringify({})
+
+
+
+        });
 
 
     }catch(error){
